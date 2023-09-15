ac524a12546596069fc710c1bb1b1926f92edb6e
devang000 <devang.gohil111734@marwadiuniversity.ac.in>
2023-09-15 10:24:46 +0530

    Add project files.

---
+++ Expense Tracker/Program.cs
@@ -0,0 +1,34 @@
+using Expense_Tracker.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Configuration;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+//DI
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DevConnection")));
+
+//Register Syncfusion lisance
+Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Ngo9BigBOggjHTQxAR8/V1NGaF1cWGhAYVJ/WmFZfV1gdVVMYFpbRHNPMyBoS35RdUVqWH1fcHRTRmVeU0Z/");
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
