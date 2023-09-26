fc8fa1dad840de773fc3064b812d02bf84e147df
devang000 <devang.gohil111734@marwadiuniversity.ac.in>
2023-09-26 23:13:48 +0530

    Dashboard Error fetching ammount

--- Expense Tracker/Models/Category.cs
+++ Expense Tracker/Models/Category.cs
@@ -14,6 +14,7 @@ namespace Expense_Tracker.Models
         public string Icon { get; set; } = "";
         [Column(TypeName = "nvarchar(10)")]
         public string Type { get; set; } = "Expense";
+        
 
         [NotMapped]
         public string? TitleWithIcon 
